16db415cc6ff355f44f60d97e22fe6eee7c26cc1
HugareBR <galegrettilima@gmail.com>
2026-09-14 20:51:20 -0300

    Update turnip_builder.sh

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -24,8 +24,7 @@ run_all(){
 	check_deps
 	prepare_workdir
 	# This has path slash in the branch name and thus needs some workarounds
-	build_lib_for_android turnip/gen8 turnip
-	build_lib_for_android turnip/gen8 turnip-sync apply
+	build_lib_for_android turnip/26.2 turnipy
 	#build_lib_for_android gen8-yuck
 }
 
